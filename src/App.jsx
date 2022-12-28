1db362ca246e65e279d3da81c30694126655d203
Guilherme Machado <machadofguilherme@gmail.com>
2022-12-28 10:05:52 -0300

    iniciando o frontend

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+const App = () => {
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const getQuote = async () => {
+      const url = 'https://quoran-api.vercel.app';
+      
+      const quote = await fetch(url);
+      const json = await quote.json();
+      setQuote(json);
+      return json;
+    }
+
+    getQuote();
+  }, []);
+
+
+  useEffect(() => {
+    const requestOperation = async () => {
+      const url = 'https://quoran-api.vercel.app/author';
+      const data = { author: "Xupeta" };
+
+      const request = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+
+      const json = await request.json();
+      console.log(json);
+    }
+
+    requestOperation();
+  }, []);
+
+  return (
+    <span>
+      <h3>{ quote.quote }</h3>
+      <p>{ quote.author}</p>
+    </span>
+  )
+}
+
+export default App;
